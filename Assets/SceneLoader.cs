d06fcce5c403e693a0f4f4eefe2e2aa75d69447f
감파른 <131964075+gampaleun@users.noreply.github.com>
2025-07-20 02:09:37 +0900

    add loading screen and other buttons
    feat: 로딩 화면 구현 및 임시 버튼 추가

--- Assets/SceneLoader.cs
+++ Assets/SceneLoader.cs
@@ -1,15 +1,47 @@
 using UnityEngine;
-using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
-public class SceneChangeButton : MonoBehaviour
+public class IntroManager : MonoBehaviour
 {
-    public string sceneName; // 전환할 씬 이름
+    public GameObject loadingPanel;
+    public GameObject mainMenuPanel;
+    public Slider loadingBar;
+    public float fakeLoadTime = 3f;
+
+    private float timer = 0f;
+    private bool isLoading = true;
 
     void Start()
     {
-        GetComponent<Button>().onClick.AddListener(() => {
-            SceneManager.LoadScene(sceneName);
-        });
+        loadingPanel.SetActive(true);
+        mainMenuPanel.SetActive(false);
+    }
+
+    void Update()
+    {
+        if (isLoading)
+        {
+            timer += Time.deltaTime;
+            loadingBar.value = timer / fakeLoadTime;
+
+            if (timer >= fakeLoadTime)
+            {
+                isLoading = false;
+                loadingPanel.SetActive(false);
+                mainMenuPanel.SetActive(true);
+            }
+        }
+    }
+
+    // 버튼 이벤트용
+    public void OnStartGame()
+    {
+        SceneManager.LoadScene("GameScene"); // 게임 씬 이름에 맞게 수정
+    }
+
+    public void OnExit()
+    {
+        Application.Quit();
     }
 }
