be1d73f06d05544c831ae561ae6f6cd4355cb04a
kseohyuz <seohyun518612@naver.com>
2025-07-30 11:45:56 +0900

    Feat: citizens 배열 기반 SetActive 로직 추가

--- Assets/CitizenGroupController.cs
+++ Assets/CitizenGroupController.cs
@@ -2,12 +2,22 @@ using UnityEngine;
 
 public class CitizenGroupController : MonoBehaviour
 {
-    public GameObject[] citizens; // 인스펙터에서 시민 10개 연결
+    public GameObject[] citizens; 
     private int currentVisibleCount = -1;
+
+    void Start()
+    {
+        // 처음 시민 
+        currentVisibleCount = 5;
+
+        for (int i = 0; i < citizens.Length; i++)
+        {
+            citizens[i].SetActive(i < currentVisibleCount);
+        }
+    }
 
     public void UpdateSatisfaction(float satisfaction)
     {
-        // 만족도(0~1)에 따라 시민 수 결정 (최소 1명 보장)
         int targetVisibleCount = Mathf.Clamp(Mathf.RoundToInt(satisfaction * (citizens.Length - 1)) + 1, 1, citizens.Length);
 
         if (targetVisibleCount != currentVisibleCount)
