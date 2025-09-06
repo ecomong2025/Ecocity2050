25ba9e3b270c0a56e78aecdc8295549392f0a027
Jiwon <onetwowon121@gmail.com>
2025-09-06 14:37:21 +0900

    Feat: GameManager 수정

--- Assets/ResourceSystem/ResourceScript/GameManager.cs
+++ Assets/ResourceSystem/ResourceScript/GameManager.cs
@@ -77,7 +77,7 @@ public class GameManager : MonoBehaviour
     public void AddBuilding(string buildingName, int cost, int co2Impact, int incomePerMinute, Vector3 position, GameObject buildingObject)
     {
         BuildingInfo newBuilding = new BuildingInfo(buildingName, cost, co2Impact, incomePerMinute, position, buildingObject);
-        builtBuildings.Add(newBuilding);;
+        builtBuildings.Add(newBuilding);
         Debug.Log($"[건물 추가] {buildingName} - 비용: {cost}, CO2: {co2Impact}, 수입: {incomePerMinute}/분");
     }
 
