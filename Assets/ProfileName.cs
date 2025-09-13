774130467e3e15f147f6283f5bc7847a8593fa05
kseohyuz <seohyun518612@naver.com>
2025-09-14 00:51:29 +0900

    feat: 카카오 로그인 후 사용자 이름 ui 반영 구현(baseurl: 로컬 버전)

---
+++ Assets/ProfileName.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using TMPro;
+
+public class ProfileName : MonoBehaviour
+{
+    public TMP_Text nameText;
+
+    void Start()
+    {
+        if (UserDataManager.Instance != null)
+        {
+            UserDataManager.Instance.onLoginSuccess += UpdateName;
+
+            // 이미 로그인된 상태면 바로 UI 갱신
+            if (UserDataManager.Instance.isLoggedIn)
+            {
+                UpdateName(UserDataManager.Instance.GetDisplayName());
+            }
+            else
+            {
+                nameText.text = "로그인이 필요합니다";
+            }
+        }
+        else
+        {
+            Debug.LogWarning("[ProfileName] UserDataManager.Instance가 null임");
+        }
+    }
+
+    void UpdateName(string displayName)
+    {
+        Debug.Log($"[ProfileName] 로그인 후 불러온 닉네임: {displayName}");
+        if (nameText != null)
+            nameText.text = displayName;
+    }
+
+    private void OnDestroy()
+    {
+        if (UserDataManager.Instance != null)
+            UserDataManager.Instance.onLoginSuccess -= UpdateName;
+    }
+}
